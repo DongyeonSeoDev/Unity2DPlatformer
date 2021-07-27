f31f0fb959feb93e6abf7644b57165906bf0e1a1
sdy362 <seodye04@gmail.com>
2021-07-28 06:25:26 +0900

    기능추가: 회전 가시 애니메이션 추가

---
+++ Assets/Scripts/Rotating Thorn/RotatingThornAnimation.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotatingThornAnimation : MonoBehaviour
+{
+    [SerializeField] private Vector3 addRotation = Vector3.zero;
+
+    private Quaternion currentRotation = Quaternion.identity;
+    private Quaternion targetRotation = Quaternion.identity;
+
+
+    private void Update()
+    {
+        currentRotation = transform.rotation;
+        targetRotation.eulerAngles = currentRotation.eulerAngles + addRotation;
+        transform.rotation = targetRotation;
+    }
+}
